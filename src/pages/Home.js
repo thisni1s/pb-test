c87318a1b6b0d25c42b17f4ae49b68c13844ea16
thisni1s <nils@jn2p.de>
2023-02-03 19:56:12 +0100

    features

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { useState, useEffect, Fragment } from 'react'
 import './App.css';
 import { useNavigate } from 'react-router-dom';
 import PocketBase from 'pocketbase';
-import { TextField, Card, CardActions, CardContent, AppBar, Fab, Tabs, Tab, Container, List, ListItem, ListItemText, ListItemAvatar, Avatar, Typography, Divider, LinearProgress, IconButton, Toolbar, Paper, Button, Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText, BottomNavigation, BottomNavigationAction } from '@mui/material';
+import { Switch, TextField, Card, CardActions, CardContent, AppBar, Fab, Tabs, Tab, Container, List, ListItem, ListItemText, ListItemAvatar, Avatar, Typography, Divider, LinearProgress, IconButton, Toolbar, Paper, Button, Dialog, DialogTitle, DialogContent, DialogActions, DialogContentText, BottomNavigation, BottomNavigationAction } from '@mui/material';
 import moment from 'moment';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
@@ -12,31 +12,34 @@ import AddIcon from '@mui/icons-material/Add';
 import CheckIcon from '@mui/icons-material/Check';
 import HistoryIcon from '@mui/icons-material/History';
 import InventoryIcon from '@mui/icons-material/Inventory';
+import CloseIcon from '@mui/icons-material/Close';
+import { apicallGet } from '../callApi';
 
 import {useGlobulContext, useUpdateGlobulContext} from '../GlobulContext'
 import { Stack } from '@mui/system';
 
 export default function Home() {
-  const pb = new PocketBase('https://base.jn2p.de');
+  const baseurl = 'https://base.jn2p.de'
+  const pb = new PocketBase(baseurl);
   const navigate = useNavigate()
   const [entries, setEntries] = useState([])
   const [wtime, setWtime] = useState({val: 0, unit: 'day', worked: 0})
+  const [tasks, setTasks] = useState([])
+  const [newDia, setNewDia] = useState(false)
   useEffect(() => {
-    async function getWorkEntries() {
-      console.log('Call me maybe')
-      try {
-        const records = await pb.collection('work_entries').getFullList(10, {
-          sort: '-created',
-          filter: 'user="'+pb.authStore.model.id+'"',
-        });
-        setEntries(records)
-      } catch (error) {
-        console.log(error)
-      }      
-    }  
+    async function taskGet() {
+      await initTasks()
+    }
+    taskGet()
+  }, [])
 
-    getWorkEntries()
-    getWorkTime()
+  useEffect(() => {
+    pb.collection('tasks').subscribe('*', function (e) {
+      handleEvent(e)
+    });
+    return function cleanup() {
+      pb.collection('tasks').unsubscribe();
+    }
   }, [])
 
   const test_tasks = [
@@ -53,112 +56,89 @@ export default function Home() {
       claimed: 'Peter',
     }
   ]
-  const [tasks, setTasks] = useState(test_tasks)
-
   async function getWorkTime() {
-    console.log('Call me maybe2')
-    try {
-      const record = await pb.collection('time_setting').getFirstListItem('user="'+pb.authStore.model.id+'"', {
-        expand: 'relField1,relField2.subRelField',
-      })
-      console.log('1')
-      //during the last month week day
-      //let time = moment().subtract(record.time_val, record.unit)
-      let time = moment().startOf(record.unit.substring(0, record.unit.length))
-      time = time.format('YYYY-MM-DD hh:mm:ss')
-      console.log('2')
-      const record2 = await pb.collection('work_entries').getFullList(100, {
-        filter: 'created >= "'+time+'" && user="'+pb.authStore.model.id+'"',
-      });
-      console.log('3')
-      let minutes = 0
-      record2.forEach(element => {
-        minutes = minutes + element.duration_min
-      });
-      setWtime({val: record.time_val, unit: record.unit, worked: minutes})
-    } catch (error) {
-      console.log(error)
-    }        
+    //TODO      
   }
 
-
-    function sanitizeTime(mins) {
-      if (mins < 60) {
-        return mins+' minutes'
-      } else {
-        return Math.round((mins/60) *100)/100+' hours'
-      }
+  async function initTasks() {
+    try {
+      const taskList = await pb.collection('tasks').getFullList(200, {
+        filter: 'spend_minutes=0',
+        expand: 'creator,claimed'
+      })
+      taskList.map(async task => {
+        return {
+          ...task,
+          username: await getUsernameForUserid(task.creator)
+        }
+      })
+      setTasks(taskList)
+    } catch (error) {
+      console.log(error)
     }
+  }
+
+  async function getUsernameForUserid(id) {
+    //return await apicallGet('GET', baseurl+'/api/st_users/'+id, pb.authStore.token)
+    return id
+  }
+
+  function handleEvent(event) {
+    console.log('i am listening, ', event)
+    setTasks(prevstate => {
+      switch (event.action) {
+        case 'create':
+          return [...prevstate, { ...event.record, username: 'testUser'}]
+        case 'delete':
+          return prevstate.filter(el => el.id !== event.record.id)
+        case 'update':
+          return prevstate.map(el => el.id === event.record.id ? { ...event.record, username: el.username } : el)      
+        default:
+          return prevstate
+      }
+    })
+  }
+
+  function sanitizeTime(mins) {
+    if (mins < 60) {
+      return mins+' minutes'
+    } else {
+      return Math.round((mins/60) *100)/100+' hours'
+    }
+  }
 
     async function deleteEntry(id) {
       try {
-        await pb.collection('work_entries').delete(id);
-        const newEntries = entries.filter(e => e.id !== id)
-        setEntries(newEntries)
-        await getWorkTime()
+        await pb.collection('tasks').delete(id);
       } catch (error) {
         console.log(error)
-      }
-      
+      }      
     }
 
     const handleChange = (event, newValue) => {
       console.log('nw: '+newValue)
       switch(newValue) {
-        case 2:
-          navigate('/Profile')
-          break
+        case 1:
+          alert("not implemented!")
+          break;
         case 0:
-          navigate('/newEntry')
-          break
-        case 3:
-          navigate('/userOverview')
           break
         default:
           break
       }
     };
 
-    const listEntries = entries.map((entry)=>{
-        if (entry !== undefined) {
-          return (
-            <>
-            <ListItem
-              key={entry.id}
-              alignItems='flex-start'
-              secondaryAction={
-                <IconButton
-                 aria-label="delete"
-                 onClick={() => deleteEntry(entry.id)}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              }
-            >
-              <ListItemAvatar>
-              <Avatar>
-              <AssignmentIcon />
-              </Avatar>
-              </ListItemAvatar>
-              <ListItemText
-                primary={entry.duration_min+' mins @ '+moment(entry.created).format('DD.MM.YYYY')} 
-                secondary={entry.description}
-              />
-              
-            </ListItem>
-            <Divider variant="inset" component="li" />
-            </>
-          )
-        } else {
-          return (<></>)
-        }
-      })
-  
     const TaskCard = props => {
       const task = props.task
       const [dialog, setDialog] = useState(false)
       const [delDia, setDelDia] = useState(false)
       const [duration, setDuration] = useState(0)
+
+      function handleDelete() {
+        deleteEntry(task.id)
+        setDelDia(false)
+      }
+
       return (
         <Grid xs={12} md={4}>
           <Card>
@@ -170,15 +150,15 @@ export default function Home() {
                 {task.description}
               </Typography>
               <Typography color="text.secondary" variant="caption" gutterBottom>
-                By: {task.by}
+                By: {task.creator}
               </Typography>
               <Stack direction="row" spacing={2} sx={{display: 'flex', mt: 1}}>
                 {
-                  task.claimed.length > 0 ? <Button diabled variant='outlined' size='small' sx={{flexGrow: 1, borderColor: 'GrayText', color: 'GrayText'}}>{task.claimed}</Button> : <Button variant='outlined' size='small' sx={{flexGrow: 1}}><AddIcon fontSize="small"/></Button>
+                  task.claimed.length > 0 ? <Button disabled variant='outlined' size='small' sx={{flexGrow: 1}}>{task.claimed}</Button> : <Button variant='outlined' size='small' sx={{flexGrow: 1}}><AddIcon fontSize="small"/></Button>
                 }                
                 <Button variant='outlined' size='small' sx={{flexGrow: 1}} onClick={() => setDialog(true)}><CheckIcon fontSize="small"/></Button>
                 {
-                  task.by === pb.authStore.model.username ? <IconButton aria-label="delete" size="small" onClick={() => setDelDia(true)}><DeleteIcon fontSize="small"/></IconButton> : <></>
+                  task.creator === pb.authStore.model.id ? <IconButton aria-label="delete" size="small" onClick={() => setDelDia(true)}><DeleteIcon fontSize="small"/></IconButton> : <></>
                 }
               </Stack>
               <Dialog open={dialog} onClose={() => setDialog(false)}>
@@ -206,8 +186,8 @@ export default function Home() {
               <Dialog open={delDia} onClose={() => setDelDia(false)}>
                 <DialogTitle>Delete {task.title} ?</DialogTitle>
                 <DialogActions>
-                  <Button variant='outlined' size='small' sx={{flexGrow: 1}} onClick={() => setDelDia(false)}>Löschen</Button>               
-                  <Button variant='outlined' size='small' sx={{flexGrow: 1}} onClick={() => {console.log('hello'); setDelDia(false); console.log(delDia)}}>Abbrechen</Button>
+                  <Button variant='outlined' size='small' sx={{flexGrow: 1}} onClick={() => {handleDelete()}}>Löschen</Button>               
+                  <Button variant='outlined' size='small' sx={{flexGrow: 1}} onClick={() => {setDelDia(false)}}>Abbrechen</Button>
                 </DialogActions>
               </Dialog>          
             </CardContent>
@@ -217,6 +197,116 @@ export default function Home() {
 
     }
 
+    const NewTask = props => {
+      const [title, setTitle] = useState("")
+      const [description, setDiscription] = useState("")
+      const [spendMin, setSpendMin] = useState(0)
+      const [done, setDone] = useState(false)
+      const [claim, setClaim] = useState(false)
+
+      async function handleClose(save) {
+        if (save) {
+          //sanity checks
+          if(title.length === 0 || (done && spendMin <= 0)) {
+            alert('Titel muss gesetzt sein. Wenn die Aufgabe schon erledigt ist muss eine Dauer angegeben werden die größer 0 ist!')
+          } else {
+            try {
+              const data = {
+                "creator": pb.authStore.model.id,
+                "claimed": (claim || done) ? pb.authStore.model.id : '',
+                "title": title,
+                "description": description,
+                "spend_minutes": spendMin
+              }
+              const record = await pb.collection('tasks').create(data);              
+            } catch (error) {
+              console.log(error)
+              alert('error creating task')
+              //setNewDia(false)
+            }
+            setNewDia(false)
+        }
+        } else {
+          setNewDia(false)
+        }
+        
+      }
+
+      return (
+        <Dialog fullScreen open={newDia} onClose={() => handleClose(false)}>
+          <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={() => handleClose(false)}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Neue Aufgabe erstellen
+            </Typography>
+            <Button autoFocus color="inherit" onClick={() => handleClose(true)}>
+              Speichern
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Stack spacing={2} sx={{mt: 2, ml: 2, mr: 2, display: 'flex'}}>
+          <TextField
+            placeholder="Titel"
+            label="Titel"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextField
+            placeholder="Description"
+            label="Description"
+            multiline
+            rows={4}
+            value={description}
+            onChange={(e) => setDiscription(e.target.value)}
+          />
+          <Stack direction="row" sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <Typography sx={{flexGrow: 1}}>Aufgabe ist bereits Erledigt</Typography>
+            <Switch
+              checked={done}
+              onChange={(e) => setDone(e.target.checked)}
+              inputProps={{ 'aria-label': 'controlled' }}
+            />
+          </Stack>
+          {
+            done ? 
+            <TextField
+              type="number"
+              placeholder="0"
+              label="Duration minutes"
+              value={spendMin}
+              onChange={(e) => setSpendMin(e.target.value)}
+              sx={{mb: 2}}
+            />
+            :
+            <Stack direction="row" sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <Typography sx={{flexGrow: 1}}>Aufgabe für mich vormerken?</Typography>
+            <Switch
+              checked={claim}
+              onChange={(e) => setClaim(e.target.checked)}
+              inputProps={{ 'aria-label': 'controlled' }}
+            />
+            </Stack>
+          }          
+        </Stack>
+        </Dialog>
+      )
+
+    }
+    
+
+    function getTaskCards() {
+      return tasks
+        .filter(task => task.spend_minutes === 0)
+        .map(task => {return <TaskCard task={task}/>})
+    }
 
     return(
     <>
@@ -237,19 +327,14 @@ export default function Home() {
           </Typography>
         </Container>
         <Grid container spacing={2}>
-          {tasks.map(task => {
-            return <TaskCard task={task}/>
-          })}
+          {getTaskCards()}
         </Grid>
-      </Stack>      
-      <Fab color="primary" aria-label="add" sx={{ position: 'absolute',  bottom: 60,  right: 20,}}>
+      </Stack>
+      <NewTask/>      
+      <Fab color="primary" aria-label="add" sx={{ position: 'absolute',  bottom: 60,  right: 20,}} onClick={() => setNewDia(true)}>
         <AddIcon />
       </Fab>
     </Container>
-    {/*<Tabs value={0} onChange={handleChange} sx={{ position: 'absolute', bottom: 0}} centered>
-        <Tab label="Home" />
-        <Tab label="History" />
-        </Tabs>*/}
     <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
       <BottomNavigation value={0} onChange={handleChange}>
         <BottomNavigationAction label="Home" icon={<InventoryIcon />} />
